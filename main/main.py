c5843638a2cc57cef860c011d043421ccf8fd3ab
Dryzera <endryusdaniel6321@gmail.com>
2024-06-27 17:49:17 -0300

    importado token e prefix

--- main/main.py
+++ main/main.py
@@ -5,23 +5,24 @@ base = {}
 PATH_IMPORT_DB = '.\\base\\db.json'
 
 class ImportBase:
-    def __init__(self, json):
-        self.import_json = json
-
     def imports(self):
         with open(PATH_IMPORT_DB, 'r', encoding='utf8') as file:
             base = json.load(file)
         return base
 
 class MyClient(discord.Client):
-    async def on_ready(self):
+    def on_ready(self):
         print(f'Logged on as {self.user}!')
 
-    async def on_message(self, message):
+    def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+
+imported = ImportBase().imports()
+TOKEN = imported['token']
+PREFIX = imported['prefix-bot']
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('my token goes here')
+client.run(TOKEN)
